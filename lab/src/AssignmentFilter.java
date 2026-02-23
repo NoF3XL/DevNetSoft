02e9e72e482933622c2e64299a8957d77d9002ce
EXFANIXTEY <evgrinchin@mail.ru>
2026-02-23 12:39:39 +0700

    complete 2.3 goal

---
+++ lab/src/AssignmentFilter.java
@@ -0,0 +1,20 @@
+import java.util.Objects;
+
+@FunctionalInterface
+public interface AssignmentFilter {
+    boolean test(RoleAssignment assignment);
+
+    default AssignmentFilter and(AssignmentFilter other) {
+        Objects.requireNonNull(other);
+        return assignment -> this.test(assignment) && other.test(assignment);
+    }
+
+    default AssignmentFilter or(AssignmentFilter other) {
+        Objects.requireNonNull(other);
+        return assignment -> this.test(assignment) || other.test(assignment);
+    }
+
+    default AssignmentFilter negate() {
+        return assignment -> !this.test(assignment);
+    }
+}
